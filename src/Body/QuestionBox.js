0061f7b5fb3d7b32f8354094d8c29d74b91aa345
ManuelHMR <manuelhmrjf@gmail.com>
2022-03-20 15:30:09 -0300

    start screen without bonus

--- src/Body/QuestionBox.js
+++ src/Body/QuestionBox.js
@@ -4,6 +4,7 @@ export default function QuestionBox({number}){
     return (
         <div className="question-box">
             <h2>Pergunta {number}</h2>
+            <ion-icon name="play-outline"></ion-icon>
         </div>
     )
 }
---
+++ src/Body/StartScreen.js
@@ -0,0 +1,17 @@
+import logo from "./../assets/logo.png"
+import "./../css/start-screen.css";
+import { useState } from "react";
+
+export default function StartScreen(){
+    const [started, setStarted] = useState(false);
+
+    return !started ?(
+        <div className="start-screen">
+            <img src={logo} alt="ZapRecalls logo"/>
+            <h1>ZapRecall</h1>
+            <button onClick={()=> setStarted(true)}>
+                Iniciar Recall!
+            </button>
+        </div>
+    ) : (``);
+}
